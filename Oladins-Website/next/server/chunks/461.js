d1ba203c15ae9caa895119738f07f1f52ce24d26
whatcost99 <isimdegistir21@gmail.com>
2023-04-24 21:44:09 +0300

    large file

---
+++ Oladins-Website/next/server/chunks/461.js
@@ -0,0 +1,26 @@
+"use strict";
+exports.id = 461;
+exports.ids = [461];
+exports.modules = {
+
+/***/ 7461:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "v": () => (/* binding */ config)
+/* harmony export */ });
+const config = {
+    title: 'OLADINS',
+    description: 'Oladins Web',
+    contractAddress: '0xF68ABb0d485b25968f29291Ec569C5DA29b565A7',
+    maxMintAmount: 10,
+    presaleMaxMintAmount: 1,
+    price: 0.0077
+};
+
+
+
+/***/ })
+
+};
+;
